35348fc87c5343390efb78cc1d7eed2bca43ddd3
ffernnando <ffernnando192@gmail.com>
2026-05-29 16:07:30 +0200

    dohvatiSve() posložen otprilike valjda ig

--- js/server/zapisiModul.cjs
+++ js/server/zapisiModul.cjs
@@ -6,15 +6,71 @@ class ZapisiModul {
     }
 
     dohvatiSve(kriteriji) {
-        var sadrzaj;
-        var linije = new Array();
-        fs.readFile(this.putanjaDatoteke, "utf-8", (greska, podaci) => {
-            if (greska) {
-                console.log(greska)
-            } else {
-                sadrzaj = podaci;
+
+        let podaci = fs.readFileSync(this.putanjaDatoteke, "utf-8");
+        if (kriteriji === undefined) {
+            kriteriji = {};
+        }
+
+        let zapisi = new Array();
+        let redovi = podaci.split("\r\n");
+
+        for (let red of redovi) {
+            let stupci = red.split("#");
+            let zapis = {
+                id: stupci[0],
+                naziv: stupci[1],
+                opis: stupci[2],
+                kategorija: stupci[3],
+                datumUnosa: stupci[4]
             }
-        })
+            zapisi.push(zapis);
+        }
+
+        /* zapisi.forEach(zapis => {
+            console.log('------------------------ ZAPIS ------------------------');
+            for (const key in zapis) {
+                console.log(`${key}: ${zapis[key]}`);
+            }
+        }); */
+
+        console.log('KRITERIJI: ');
+        for (let key in kriteriji) {
+            console.log(`${key}: ${kriteriji[key]}`);                    
+        }
+            
+        let filtriraniZapisi = new Array();
+
+        for (let zapis of zapisi) {
+            let prolazno = true;
+            if (kriteriji.naziv !== undefined && kriteriji.naziv !== "") {
+                if (!zapis.naziv.toLowerCase().includes(kriteriji.naziv.toLowerCase())) {
+                    prolazno = false;
+                }
+            }
+
+            if (kriteriji.opis !== undefined && kriteriji.opis !== "") {
+                if (!zapis.opis.toLowerCase().includes(kriteriji.opis.toLowerCase())) {
+                    prolazno = false;
+                }
+            }
+
+            if (kriteriji.kategorija !== undefined && kriteriji.kategorija !== "") {
+                if (!zapis.kategorija.toLowerCase().includes(kriteriji.kategorija.toLowerCase())) {
+                    prolazno = false;
+                }
+            }
+
+            if (prolazno === true) {
+                filtriraniZapisi.push(zapis);
+            }
+        }
+
+        /* for (let k in filtriraniZapisi) {
+            console.log(`${k}: ${filtriraniZapisi[k]}`);
+        } */
+
+        return filtriraniZapisi;
 
 
 
